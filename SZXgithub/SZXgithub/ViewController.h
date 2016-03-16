2685184719145b9a737f5f66fb137624302d1b61
wyl <1203302261@qq.com>
2016-03-16 10:16:44 +0800

    搭建工程

---
+++ SZXgithub/SZXgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZXgithub
+//
+//  Created by Umbrella company on 16/3/16.
+//  Copyright © 2016年 Umbrella company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SZXgithub/SZXgithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SZXgithub
+//
+//  Created by Umbrella company on 16/3/16.
+//  Copyright © 2016年 Umbrella company. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
